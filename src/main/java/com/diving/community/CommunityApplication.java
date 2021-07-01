1e2953b6423b04453d4748dbfcc3b0512b0d97e2
Kimyechan <vlvkcjswo7@naver.com>
2021-07-01 15:19:15 +0900

    Conf: Applying Database Kafka Config
    import config yml

--- src/main/java/com/diving/community/CommunityApplication.java
+++ src/main/java/com/diving/community/CommunityApplication.java
@@ -1,13 +1,19 @@
 package com.diving.community;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
 public class CommunityApplication {
+    private static final String APPLICATION_LOCATIONS = "spring.config.location="
+            + "classpath:application.yml,"
+            + "classpath:database.yml,"
+            + "classpath:kafka.yml";
 
     public static void main(String[] args) {
-        SpringApplication.run(CommunityApplication.class, args);
+        new SpringApplicationBuilder(CommunityApplication.class)
+                .properties(APPLICATION_LOCATIONS)
+                .run(args);
     }
 
 }
